68a97afa5e5d03ddcf8620e6ad68c046c87de375
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-20 09:33:17 +0300

    home form design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,10 +35,47 @@
             lblFootername = new Label();
             label1 = new Label();
             panelOPositive = new Panel();
+            lblDonors = new Label();
+            lblPositiveCount = new Label();
             lblBloodGroup = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            lblOnegativecount = new Label();
+            lblONegative = new Label();
+            panel2 = new Panel();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            panel3 = new Panel();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            panel4 = new Panel();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            panel5 = new Panel();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            panel6 = new Panel();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            panel7 = new Panel();
+            label20 = new Label();
+            label21 = new Label();
+            label22 = new Label();
             menuStripTop.SuspendLayout();
             panelFooter.SuspendLayout();
             panelOPositive.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
             SuspendLayout();
             // 
             // menuStripTop
@@ -99,21 +136,354 @@
             // panelOPositive
             // 
             panelOPositive.BackColor = SystemColors.Control;
+            panelOPositive.Controls.Add(lblDonors);
+            panelOPositive.Controls.Add(lblPositiveCount);
             panelOPositive.Controls.Add(lblBloodGroup);
-            panelOPositive.Location = new Point(28, 74);
+            panelOPositive.Location = new Point(25, 60);
             panelOPositive.Name = "panelOPositive";
             panelOPositive.Size = new Size(200, 100);
             panelOPositive.TabIndex = 2;
             // 
+            // lblDonors
+            // 
+            lblDonors.AutoSize = true;
+            lblDonors.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDonors.ForeColor = Color.FromArgb(226, 76, 58);
+            lblDonors.Location = new Point(96, 59);
+            lblDonors.Name = "lblDonors";
+            lblDonors.Size = new Size(81, 24);
+            lblDonors.TabIndex = 4;
+            lblDonors.Text = "Donors";
+            // 
+            // lblPositiveCount
+            // 
+            lblPositiveCount.AutoSize = true;
+            lblPositiveCount.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPositiveCount.ForeColor = Color.FromArgb(226, 76, 58);
+            lblPositiveCount.Location = new Point(116, 13);
+            lblPositiveCount.Name = "lblPositiveCount";
+            lblPositiveCount.Size = new Size(37, 40);
+            lblPositiveCount.TabIndex = 3;
+            lblPositiveCount.Text = "0";
+            // 
             // lblBloodGroup
             // 
             lblBloodGroup.AutoSize = true;
-            lblBloodGroup.Font = new Font("Harrington", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblBloodGroup.Location = new Point(14, 10);
+            lblBloodGroup.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblBloodGroup.ForeColor = Color.FromArgb(226, 76, 58);
+            lblBloodGroup.Location = new Point(3, 31);
             lblBloodGroup.Name = "lblBloodGroup";
-            lblBloodGroup.Size = new Size(29, 20);
+            lblBloodGroup.Size = new Size(70, 52);
             lblBloodGroup.TabIndex = 3;
             lblBloodGroup.Text = "O+";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(lblOnegativecount);
+            panel1.Controls.Add(lblONegative);
+            panel1.Location = new Point(265, 60);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 100);
+            panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(226, 76, 58);
+            label2.Location = new Point(96, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 24);
+            label2.TabIndex = 4;
+            label2.Text = "Donors";
+            // 
+            // lblOnegativecount
+            // 
+            lblOnegativecount.AutoSize = true;
+            lblOnegativecount.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblOnegativecount.ForeColor = Color.FromArgb(226, 76, 58);
+            lblOnegativecount.Location = new Point(116, 13);
+            lblOnegativecount.Name = "lblOnegativecount";
+            lblOnegativecount.Size = new Size(37, 40);
+            lblOnegativecount.TabIndex = 3;
+            lblOnegativecount.Text = "0";
+            // 
+            // lblONegative
+            // 
+            lblONegative.AutoSize = true;
+            lblONegative.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblONegative.ForeColor = Color.FromArgb(226, 76, 58);
+            lblONegative.Location = new Point(3, 31);
+            lblONegative.Name = "lblONegative";
+            lblONegative.Size = new Size(69, 52);
+            lblONegative.TabIndex = 3;
+            lblONegative.Text = "O-";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.Control;
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label7);
+            panel2.Location = new Point(265, 181);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 100);
+            panel2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(226, 76, 58);
+            label5.Location = new Point(96, 59);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Donors";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(226, 76, 58);
+            label6.Location = new Point(116, 13);
+            label6.Name = "label6";
+            label6.Size = new Size(37, 40);
+            label6.TabIndex = 3;
+            label6.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.FromArgb(226, 76, 58);
+            label7.Location = new Point(3, 31);
+            label7.Name = "label7";
+            label7.Size = new Size(70, 52);
+            label7.TabIndex = 3;
+            label7.Text = "O+";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.Control;
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(label9);
+            panel3.Controls.Add(label10);
+            panel3.Location = new Point(28, 181);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(200, 100);
+            panel3.TabIndex = 5;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.FromArgb(226, 76, 58);
+            label8.Location = new Point(96, 59);
+            label8.Name = "label8";
+            label8.Size = new Size(81, 24);
+            label8.TabIndex = 4;
+            label8.Text = "Donors";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.FromArgb(226, 76, 58);
+            label9.Location = new Point(116, 13);
+            label9.Name = "label9";
+            label9.Size = new Size(37, 40);
+            label9.TabIndex = 3;
+            label9.Text = "0";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.FromArgb(226, 76, 58);
+            label10.Location = new Point(3, 31);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 52);
+            label10.TabIndex = 3;
+            label10.Text = "O+";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.Control;
+            panel4.Controls.Add(label11);
+            panel4.Controls.Add(label12);
+            panel4.Controls.Add(label13);
+            panel4.Location = new Point(28, 433);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(200, 100);
+            panel4.TabIndex = 8;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.FromArgb(226, 76, 58);
+            label11.Location = new Point(96, 59);
+            label11.Name = "label11";
+            label11.Size = new Size(81, 24);
+            label11.TabIndex = 4;
+            label11.Text = "Donors";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.FromArgb(226, 76, 58);
+            label12.Location = new Point(116, 13);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 40);
+            label12.TabIndex = 3;
+            label12.Text = "0";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.FromArgb(226, 76, 58);
+            label13.Location = new Point(3, 31);
+            label13.Name = "label13";
+            label13.Size = new Size(70, 52);
+            label13.TabIndex = 3;
+            label13.Text = "O+";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.Control;
+            panel5.Controls.Add(label14);
+            panel5.Controls.Add(label15);
+            panel5.Controls.Add(label16);
+            panel5.Location = new Point(265, 433);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(200, 100);
+            panel5.TabIndex = 10;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.ForeColor = Color.FromArgb(226, 76, 58);
+            label14.Location = new Point(96, 59);
+            label14.Name = "label14";
+            label14.Size = new Size(81, 24);
+            label14.TabIndex = 4;
+            label14.Text = "Donors";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.ForeColor = Color.FromArgb(226, 76, 58);
+            label15.Location = new Point(116, 13);
+            label15.Name = "label15";
+            label15.Size = new Size(37, 40);
+            label15.TabIndex = 3;
+            label15.Text = "0";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label16.ForeColor = Color.FromArgb(226, 76, 58);
+            label16.Location = new Point(3, 31);
+            label16.Name = "label16";
+            label16.Size = new Size(70, 52);
+            label16.TabIndex = 3;
+            label16.Text = "O+";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.Control;
+            panel6.Controls.Add(label17);
+            panel6.Controls.Add(label18);
+            panel6.Controls.Add(label19);
+            panel6.Location = new Point(265, 312);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(200, 100);
+            panel6.TabIndex = 9;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label17.ForeColor = Color.FromArgb(226, 76, 58);
+            label17.Location = new Point(96, 59);
+            label17.Name = "label17";
+            label17.Size = new Size(81, 24);
+            label17.TabIndex = 4;
+            label17.Text = "Donors";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.ForeColor = Color.FromArgb(226, 76, 58);
+            label18.Location = new Point(116, 13);
+            label18.Name = "label18";
+            label18.Size = new Size(37, 40);
+            label18.TabIndex = 3;
+            label18.Text = "0";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label19.ForeColor = Color.FromArgb(226, 76, 58);
+            label19.Location = new Point(3, 31);
+            label19.Name = "label19";
+            label19.Size = new Size(70, 52);
+            label19.TabIndex = 3;
+            label19.Text = "O+";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.Control;
+            panel7.Controls.Add(label20);
+            panel7.Controls.Add(label21);
+            panel7.Controls.Add(label22);
+            panel7.Location = new Point(25, 312);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(200, 100);
+            panel7.TabIndex = 7;
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Font = new Font("Harrington", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label20.ForeColor = Color.FromArgb(226, 76, 58);
+            label20.Location = new Point(96, 59);
+            label20.Name = "label20";
+            label20.Size = new Size(81, 24);
+            label20.TabIndex = 4;
+            label20.Text = "Donors";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Font = new Font("Harrington", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label21.ForeColor = Color.FromArgb(226, 76, 58);
+            label21.Location = new Point(116, 13);
+            label21.Name = "label21";
+            label21.Size = new Size(37, 40);
+            label21.TabIndex = 3;
+            label21.Text = "0";
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Font = new Font("Harrington", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label22.ForeColor = Color.FromArgb(226, 76, 58);
+            label22.Location = new Point(3, 31);
+            label22.Name = "label22";
+            label22.Size = new Size(70, 52);
+            label22.TabIndex = 3;
+            label22.Text = "O+";
             // 
             // frmHome
             // 
@@ -121,6 +491,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
             ClientSize = new Size(1135, 610);
+            Controls.Add(panel4);
+            Controls.Add(panel5);
+            Controls.Add(panel6);
+            Controls.Add(panel7);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(panelOPositive);
             Controls.Add(panelFooter);
             Controls.Add(menuStripTop);
@@ -135,6 +512,20 @@
             panelFooter.PerformLayout();
             panelOPositive.ResumeLayout(false);
             panelOPositive.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -149,5 +540,35 @@
         private Label lblFootername;
         private Panel panelOPositive;
         private Label lblBloodGroup;
+        private Label lblDonors;
+        private Label lblPositiveCount;
+        private Panel panel1;
+        private Label label2;
+        private Label lblOnegativecount;
+        private Label lblONegative;
+        private Panel panel2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Panel panel3;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Panel panel4;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Panel panel5;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Panel panel6;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Panel panel7;
+        private Label label20;
+        private Label label21;
+        private Label label22;
     }
 }
