3ea9a1a94757fd3d244d19644be35703912100f5
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-20 07:02:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Blood_Bank_Management_system
+{
+    partial class frmHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStripTop = new MenuStrip();
+            usersToolStripMenuItem = new ToolStripMenuItem();
+            donorsToolStripMenuItem = new ToolStripMenuItem();
+            menuStripTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStripTop
+            // 
+            menuStripTop.Font = new Font("Harrington", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            menuStripTop.ImageScalingSize = new Size(20, 20);
+            menuStripTop.Items.AddRange(new ToolStripItem[] { usersToolStripMenuItem, donorsToolStripMenuItem });
+            menuStripTop.Location = new Point(0, 0);
+            menuStripTop.Name = "menuStripTop";
+            menuStripTop.Size = new Size(1135, 28);
+            menuStripTop.TabIndex = 0;
+            menuStripTop.Text = "menuStrip1";
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(70, 24);
+            usersToolStripMenuItem.Text = "Users";
+            // 
+            // donorsToolStripMenuItem
+            // 
+            donorsToolStripMenuItem.Name = "donorsToolStripMenuItem";
+            donorsToolStripMenuItem.Size = new Size(85, 24);
+            donorsToolStripMenuItem.Text = "Donors";
+            // 
+            // frmHome
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ScrollBar;
+            ClientSize = new Size(1135, 610);
+            Controls.Add(menuStripTop);
+            FormBorderStyle = FormBorderStyle.None;
+            MainMenuStrip = menuStripTop;
+            Name = "frmHome";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home";
+            menuStripTop.ResumeLayout(false);
+            menuStripTop.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStripTop;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem donorsToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Blood_Bank_Management_system
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
